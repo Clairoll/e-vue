08ad24c8e7ad646f2b584bc82b2e2197c2f0d31e
Clairoll <1755033445@qq.com>
2018-10-14 20:15:38 +0800

    a project

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="menu-wrapper"></div>
+    <div class="food-wrapper"></div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default{
+
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
